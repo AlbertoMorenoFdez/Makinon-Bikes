42854f04f33070051165a5eb9a1b3eb077d2de62
Joseaholgado <115182985+Joseaholgado@users.noreply.github.com>
2024-05-08 00:41:58 +0200

    -Obtención de token
    -Se crea servicio que detecta el token del usuario registrado y se almacena en localstorage
    -Se hacen pruebas con las rutas

--- angular-bikes/src/app/app.config.ts
+++ angular-bikes/src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
               provideAnimationsAsync(),
--- angular-bikes/src/app/components/formulario/formulario.component.ts
+++ angular-bikes/src/app/components/formulario/formulario.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { DatePickerComponent } from '../date-picker/date-picker.component';
+import { TimepickerComponent } from '../timepicker/timepicker.component';
+import { SubirArchivoComponent } from '../subir-archivo/subir-archivo.component';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule,
+            DatePickerComponent,
+            TimepickerComponent,
+            SubirArchivoComponent
+  ],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
